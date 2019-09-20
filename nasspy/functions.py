ec87c83ef443f1838c896a2bb2624587ab460b09
Travis Williams <Travis.williams@colorado.edu>
2019-09-20 14:21:24 -0600

    Update functions.py

--- nasspy/functions.py
+++ nasspy/functions.py
@@ -9,41 +9,6 @@ def dp(path):
     return os.path.join(_root, 'data', path)
 
 class nass_api:
-    '''
-    This class just builds queries and returns data from the NASS API.
-    The structure of the query is based on what, when, and where questions.
-    You can also filter the results by appending operators to the parameter.
-    The three types of queries are separated into the methods
-    get_parameter_options, get_query_count, and get_query.
-
-    They only allow requests with less than 50,000 entries, which is rather
-    small. For the whole thing go here!:
-        ftp://ftp.nass.usda.gov/quickstats
-
-    To use:
-        1) Create an api object:
-            api = NASS_API(key=<your nass key as a string>)
-        2) Get a list of available parameters with api.all_parameters
-        3) Get a list of the definitions of these parameters using
-            api.what_parameters, api.where_parameters, and api.when_parameters
-        4) Get a list of input options for any one parameter using
-            api.get_parameter_options('parameter')
-        5) Build a query as a list of strings like this:
-            ['<parameter1>=<input1>', '<parameter2>=<input2>', ...]
-        6) Filter each query by appending an operator to each string element:
-            '<parameter><operator>=<input>'
-        7) For count use the query list as the argument in api.get_query_count:
-            data = api.get_query_count(query)
-        8) For the data itself use the query list in api.get_query:
-            data = api.get_query(query)
-        9) Examples:
-            Retrieve weekly corn yields from Montana since 1980:
-
-                query = ['state_name=MONTANA', 'commodity_desc=CORN',
-                         'year__GE=1980', 'freq_desc=WEEKLY']
-                data = api.get_query(query)
-
-    '''
     def __init__(self, keypath='~/.keys/nass_api_key.txt', key=None):
         self.sample_query = ['commodity_desc=CORN', 'year__GE=2010',
                              'state_alpha=VA']
@@ -91,10 +56,6 @@ class nass_api:
               "__LIKE = like\n__NOT_LIKE = not like\n__NE = not equal ")
 
     def get_parameter_options(self, parameter):
-        '''
-        Here I want to list an example entry for each parameter, so we don't
-        have to go to the quick (not quick) stats tool to get formatting right
-        '''
         base = '/'.join([self.url, 'get_param_values', '?key=']) + self.key
         request = '&'.join([base, 'param=' + parameter])
         r = requests.get(request)
